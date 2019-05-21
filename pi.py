9050b9578876b3c82737b20a6468baf1d06a426e
Micha den Heijer <micha@michadenheijer.com>
2019-05-21 21:28:13 +0200

    Added faster calculations, and test with decimal

--- pi.py
+++ pi.py
@@ -1,13 +1,21 @@
 import math
+import time
+import sys
 
 def main():
-    sigma=0
+    start=time.time()
+    digits=5
+    sigma=0.0
     n=1
-    while True:
+    pi=0
+    multiply=10**digits
+    equal=int(math.pi*(10**digits))
+    while int(pi*multiply+0.5) != equal:
         sigma = sigma+(1/math.pow(n, 2))
         pi = math.sqrt(6*sigma)
         n+=1
-        print (pi)
+    now=time.time()
+    print("Calculated Pi correctly up to {} digits in: {} seconds using floats and Python {}".format(digits, now-start, sys.version))
 
 if __name__ == "__main__":
     main()
---
+++ speedtest.sh
@@ -0,0 +1,5 @@
+rm -rf __pycache__
+python pi.py
+python pi-decimal.py
+python3 pi.py
+python3 pi-decimal.py
