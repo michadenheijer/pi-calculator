60648b64a7c8a2b981acf9d0e03eb618d16587c9
Micha den Heijer <micha@michadenheijer.com>
2019-05-22 21:24:45 +0200

    Added PHP script

---
+++ php/pi.php
@@ -0,0 +1,26 @@
+<?php
+
+function microtime_float()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
+
+$n=1;
+$sigma=0.0;
+$pi=0.0;
+$piconstant=3.14159265358979323846;
+$digits=5;
+$time_start = microtime_float();
+
+while((int)($pi*10**$digits+0.5)!=(int)($piconstant*10**$digits+0.5)){
+    $sigma = $sigma + 1/($n*$n);
+    $pi = sqrt(6.0 * $sigma);
+    $n=$n+1;
+}
+
+$time_end = microtime_float();
+$time = $time_end - $time_start;
+$version = phpversion();
+print "Calculated Pi correctly up to $digits digits in $time seconds using PHP $version\n";
+?>
--- speedtest.sh
+++ speedtest.sh
@@ -4,3 +4,4 @@ python python/pi-decimal.py
 python3 python/pi.py
 python3 python/pi-decimal.py
 perl perl/pi.pl
+php php/pi.php
