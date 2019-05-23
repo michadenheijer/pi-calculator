24227c83e20ff809229ad5128ec5b1d99ce129ba
Micha den Heijer <micha@michadenheijer.com>
2019-05-23 21:42:55 +0200

    Added more optimazations for Perl

--- perl/pi.pl
+++ perl/pi.pl
@@ -8,9 +8,10 @@ my $pi = 0.0;
 my $piConstant = 3.14159265358979323846;
 my $digits = 5;
 my $constant = 10**$digits;
+my $equal = int($piConstant*$constant + 0.5);
 my $start = Time::HiRes::gettimeofday();
 
-while(int($pi*$constant+0.5)!=int($piConstant*$constant+ 0.5)){
+while(int($pi*$constant+0.5)!=$equal){
   $sigma = $sigma + 1/($n * $n);
   $pi = sqrt($sigma * 6.0);
   $n++;
