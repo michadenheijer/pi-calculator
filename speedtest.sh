abc8960b7510f0adb4e0b556474f4f55dc6a9a4f
Micha den Heijer <micha@michadenheijer.com>
2019-05-23 20:49:03 +0200

    Fixed order for fairer speeds

--- speedtest.sh
+++ speedtest.sh
@@ -1,7 +1,9 @@
 rm -rf __pycache__
-python python/pi.py
 python python/pi-decimal.py
-python3 python/pi.py
+python python/pi.py
 python3 python/pi-decimal.py
+python3 python/pi.py
 perl perl/pi.pl
 php php/pi.php
+gcc -o c/pi c/pi.c -lm
+./c/pi
