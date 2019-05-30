4b3e47868534f1a81ae69ae3c3c543b603283b9e
Micha den Heijer <micha@michadenheijer.com>
2019-05-30 17:42:17 +0200

    Added calculation for long double

--- c/c.sh
+++ c/c.sh
@@ -1,2 +1,4 @@
 gcc -o c/pi c/pi.c -lm
+gcc -o c/pi-long c/pi-long.c -lm
 ./c/pi
+./c/pi-long
--- c/pi-long.c
+++ c/pi-long.c
@@ -5,29 +5,29 @@
 clock_t start, end;
 double cpu_time_used;
 
-int rounding(double x){
+int rounding(long double x){
    if (x < 0.0)
      return (int)(x - 0.5);
    else
      return (int)(x + 0.5);
 }
 
-double calculate_pi(double digits){
-  double i = 1;
-  double s = 0.0;
-  double pi = 0.0;
+double calculate_pi(int digits){
+  long double i = 1.0;
+  long double s = 0.0;
+  long double pi = 0.0;
   double actual_pi = 3.14159265358979323846;
   int stop_number = (int) floor(actual_pi * pow(10, digits));
-  double sig = pow(10, digits);
+  long double sig = powl(10.0, digits);
   start = clock();
   while(rounding(pi*sig) != stop_number){
-    s = s + 1/(i*i);
-    i++;
-    pi = sqrt(6.0 * s);
+    s = s + powl(i, -2);
+    i = i + 1.0;
+    pi = sqrtl(6.0 * s);
   }
   end = clock();
   cpu_time_used = ((double) (end - start))/CLOCKS_PER_SEC;
-  printf("C double: %.4f\n", cpu_time_used);
+  printf("C long double: %.4f\n", cpu_time_used);
   return 0;
 }
 
